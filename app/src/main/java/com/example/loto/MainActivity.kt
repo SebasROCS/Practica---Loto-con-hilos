449716f21bf849b5b128c6e4b8d77ec791bdd70e
SebasROCS <sebasrdz88@gmail.com>
2024-10-29 13:30:14 -0600

    Primer commit Subida al repositorio

--- app/src/main/java/com/example/loto/MainActivity.kt
+++ app/src/main/java/com/example/loto/MainActivity.kt
@@ -14,7 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.loto.ui.theme.LotoTheme
 import com.example.loto.viewModels.LoteriaViewModels
-import com.example.loto.views.LoteriaView
+import com.example.loto.views.LoteriaView // hola
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
